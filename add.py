df2f29593fd7f69e48a7a85ad1128ec689b38e52
AKSHAY SUSANTH <26317@fisat>
2019-05-15 05:10:24 -0400

    create a funtion to multiply 2 numbers

--- add.py
+++ add.py
@@ -2,13 +2,5 @@ def add2(num1, num2):
 	s=num1 + num2
 	return s
 
-def add3(n1,n2, n3):
-	s=n1 + n2 + n3 
-	return s
-def add4(n1,n2, n3, n4):
-	s=n1 + n2 + n3 + n4
-	return s
-
 print add2(3, 4)
-print add3(3, 4, 5)
 
---
+++ multiply.py
@@ -0,0 +1,5 @@
+def pro(n1, n2):
+	p= n1 * n2
+	return p
+
+print pro(3, 4)
